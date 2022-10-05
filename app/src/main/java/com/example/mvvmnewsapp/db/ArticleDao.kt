cc91b4f7863f8f3785be645ef2b8ecb9715a6903
sam <sun.mo1088@gmail.com>
2022-10-05 11:09:31 -0600

    set up DAO and Article

---
+++ app/src/main/java/com/example/mvvmnewsapp/db/ArticleDao.kt
@@ -0,0 +1,34 @@
+package com.example.mvvmnewsapp.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.mvvmnewsapp.ui.Article
+
+//DAO - Data Access Object
+//It is like NewsAPI interface. We define the function how we can access local database
+
+@Dao
+interface ArticleDao {
+
+    //onConflict strategy determine if the article we want to save is already exist.
+    //In that case, we simply want to replace
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    //it return Long that is the ID that was inserted
+    suspend fun upsert(article: Article): Long
+
+
+    @Query("SELECT * FROM articles")
+    //here use normal function not suspend function because
+    //this function will return a livedata object that does not work with suspend function.
+    //LiveData<List<Article>> means
+    //whenever an article inside of that list change then this livedata will notify all of
+    //its observers that subscribed to changes of changes of that livedata.
+    fun getAllArticles(): LiveData<List<Article>>
+
+    @Delete
+    suspend fun deleteArticle(article: Article)
+}
