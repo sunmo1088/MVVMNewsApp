20864abb771539f2eb94a00988ff3b340f1a67f6
sam <sun.mo1088@gmail.com>
2022-10-05 11:06:08 -0600

    set up DAO and Article

--- app/src/main/java/com/example/mvvmnewsapp/ui/Article.kt
+++ app/src/main/java/com/example/mvvmnewsapp/ui/Article.kt
@@ -1,7 +1,18 @@
 package com.example.mvvmnewsapp.ui
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
+// Entity annotation tells this Article class is a table in our database
+@Entity(
+    tableName = "articles"
+)
 data class Article(
+    //set id nullable because not every article need id.
+    //We don't save every article in local database.
+    //autoGenerate inclement id
+    @PrimaryKey(autoGenerate = true)
+    var id: Int? = null,
     val author: String,
     val content: String,
     val description: String,
